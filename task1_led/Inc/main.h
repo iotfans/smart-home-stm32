5d138f48bc2fcc0f7e53aab19316c5e9f2ddfdde
iotlab-520 <iotlab520@qq.com>
2020-01-28 20:28:43 +0800

    v0.1 released

--- task1_led/Inc/main.h
+++ task1_led/Inc/main.h
@@ -60,10 +60,11 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
-#define USART_TX_Pin GPIO_PIN_2
-#define USART_TX_GPIO_Port GPIOA
-#define USART_RX_Pin GPIO_PIN_3
-#define USART_RX_GPIO_Port GPIOA
+#define B1_EXTI_IRQn EXTI15_10_IRQn
+#define USART2_TX_Pin GPIO_PIN_2
+#define USART2_TX_GPIO_Port GPIOA
+#define USART2_RX_Pin GPIO_PIN_3
+#define USART2_RX_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
