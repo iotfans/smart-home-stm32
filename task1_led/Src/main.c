24175b2ae7a9c03347acdfadf6c757db17a8c9db
iotfans <iotlab520@gmail.com>
2020-01-29 18:29:51 +0800

    old task1 add "#include <stdio.h>"
    Describe as above...

--- task1_led/Src/main.c
+++ task1_led/Src/main.c
@@ -25,7 +25,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
